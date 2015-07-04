1301b216d28e76c3e838c361c5812a826786c4c6
Amadeus Chia <foodpunch@gmai.com>
2015-07-04 16:51:10 +0800

    Base AI Abstract has issues. Roaming part works, and it's in a script called WanderingScripte. Yes that's not a typo.

--- Assets/Scripts/Anima-Spirit/AIbase.cs
+++ Assets/Scripts/Anima-Spirit/AIbase.cs
@@ -1,40 +1,93 @@
 ﻿using UnityEngine;
 using System.Collections;
 
+[RequireComponent (typeof( NavMeshAgent))]
+[RequireComponent(typeof(Rigidbody))]
+//[RequireComponent(typeof(CharacterController))]
+
+
 public abstract class AIbase : MonoBehaviour {
 
 	bool autoFire = false;
 	bool isPossessed = false;
+
+   public enum States
+    {
+        idle,
+        walk,
+        rotate,
+        retreat,
+        possessed,
+        pursue
+    };
+
+    public States AIState = States.idle;
+    float idleTime = 3.0f;
+    bool AIReady = false;
+
+    Transform spawnPoint;
+    NavMeshAgent AIBody;
+    CharacterController moveControl;
+    Rigidbody rb;
+    float distance;
 	 
 	protected abstract void ActivateAbility();
 	protected abstract void PassiveAbility();
 	// Use this for initialization
-	void Start () {
-	
-	}
-	
+    void Start()
+    {
+        spawnPoint = GameObject.FindGameObjectWithTag("SpawnPoint").GetComponent<Transform>();
+        AIBody = GetComponent<NavMeshAgent>();
+        rb = GetComponent<Rigidbody>();
+
+    }
 	// Update is called once per frame
 	void Update () {
+        Roam();
 		PassiveAbility ();
-		if (!isPossessed) {
-			Roam ();
-		}
-		else
-		{
-			CheckInput ();
-
-		}
-		gameObject.GetComponent<CharacterController>().enabled = isPossessed;
-
-
-
-
-	
+        
+        //if (!isPossessed) {
+        //    Roam ();
+        //}
+        //else
+        //{
+        //    CheckInput ();
+        //}
+			
 	}
 
 	void Roam()
 	{
 		//logic for AI roaming
+        switch (AIState)
+        {
+            case States.idle:
+                //plays AI specific Animations
+              //  AIState = States.walk;
+                AIBody.Move(transform.forward * 100);
+                print("hey");
+                break;
+            case States.walk:
+                //walks forward for a set amount of time (random distance)
+                gameObject.transform.localPosition += transform.forward*100;
+                break;
+            case States.rotate:
+                //rotates for set amount of time (random angle)
+                break;
+            case States.retreat:
+                //runs back to start
+                break;
+            case States.possessed:
+                //logic for when possessed. Most probably disabling the charactercontroller of AI
+                //with this, the functions below may need to obe placed in here.
+                break;
+            case States.pursue:
+                //faces player and walks toward player. additional feature, may be discarded. Good to have.
+                break;
+
+
+        }
+
 	}
 
 	void CheckInput()
@@ -70,6 +123,19 @@ public abstract class AIbase : MonoBehaviour {
 
 		}
 	}
+
+    //void GoodMovement()
+    //{
+    //    Vector3 targetVelocity = new Vector3(h1, 0, v1);
+    //    targetVelocity.Normalize();
+    //    targetVelocity *= speed;
+    //    Vector3 v = _rigidbody.velocity;
+    //    Vector3 velocityChange = targetVelocity - v;
+    //    velocityChange = new Vector3(Mathf.Clamp(velocityChange.x, -maxVelocityChange, maxVelocityChange), 0, Mathf.Clamp(velocityChange.z, -maxVelocityChange, maxVelocityChange));
+
+    //    _rigidbody.AddForce(velocityChange, ForceMode.VelocityChange);
+
+    //}
 
 
 }
