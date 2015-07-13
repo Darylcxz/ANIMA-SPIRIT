6cb18e24717344875b79cb064336034f7264b60a
Amadeus Chia <foodpunch@gmai.com>
2015-07-13 13:43:34 +0800

    Added roll animations and attacks

--- Assets/Scripts/Anima-Spirit/MovementController.cs
+++ Assets/Scripts/Anima-Spirit/MovementController.cs
@@ -3,7 +3,7 @@ using System.Collections;
 
 public class MovementController : MonoBehaviour {
 
-    enum States
+   public enum States
     {
         idle,
         move,
@@ -11,39 +11,68 @@ public class MovementController : MonoBehaviour {
         swing,
         jump,
         roll,
+        possess,
     };
 
-    States charStates = States.idle;
+   public States charStates = States.idle;
+   Animator _anim;
+
+    //input stuff
 
     bool roll;
     bool attack;
     bool jump;
+    bool possess;
     float hMove;
     float vMove;
 
-    bool isGrounded;
-    bool ready;
+    int attackMode = 0; //1: Stab, 2: swing
+
+    float groundDist;
+    float waitTime = 0.5f;
+    bool ready = false;
+    bool isRolling = false;
+
+    public float speed = 15.0f;
+    public float smoothDamp = 15.0f;
+
+    Rigidbody _rigidBody;
+
+    Vector3 groundPos;
+    Vector3 playerPos;
 
 
 	// Use this for initialization
 	void Start () {
+        _rigidBody = gameObject.GetComponent<Rigidbody>();
+        groundDist = gameObject.GetComponent<Collider>().bounds.center.y;
+        _anim = gameObject.GetComponent<Animator>();
         
-        roll = Input.GetKeyDown(KeyCode.LeftShift);
-        attack = Input.GetMouseButtonDown(0);
-        jump = Input.GetKeyDown(KeyCode.Space);
-        hMove = Input.GetAxis("Horizontal");
-        vMove = Input.GetAxis("Vertical");
+        
+        
 	
 	}
 	
 	// Update is called once per frame
-	void Update () {
-
-
+	void FixedUpdate () {
+        CheckInput();
+        Debug.Log(attackMode + "" + ready);
+        _anim.SetFloat("speed", _rigidBody.velocity.magnitude); // changes anim speed value to make it play move anim
+        _anim.SetInteger("attack", attackMode); //1: stab, 2:swing
+        _anim.SetBool("isRolling", isRolling);//change param to be the same as bool isRolling
         switch (charStates)
         {
             case States.idle:
-                if (jump)
+            //check if player is grounded
+                attackMode = 0;
+                ready = false;
+                isRolling = false;
+                if (vMove != 0f || hMove != 0f)
+                {
+                    charStates = States.move;
+                   
+                }
+                if (jump && isGrounded())
                 {
                     charStates = States.jump;
                 }
@@ -54,20 +83,108 @@ public class MovementController : MonoBehaviour {
                 if (roll)
                 {
                     charStates = States.roll;
+                    isRolling = true;
                 }
                 break;
             case States.move:
+                RotatingLogic(hMove, vMove);
+                MovementLogic();
+                attackMode = 0;
+                if (vMove == 0 && hMove ==0)
+                {
+                    charStates = States.idle;
+                }
+                if (jump && isGrounded())
+                {
+                    charStates = States.jump;
+                }
                 break;
             case States.jump:
+                _rigidBody.AddForce(Vector3.up*5,ForceMode.Impulse);
+                if (!isGrounded())
+                {
+                    charStates = States.idle;
+                }           
+                break;
+            case States.possess:
                 break;
             case States.roll:
+                
+                if (!isRolling)
+                {
+                    charStates = States.idle;
+                }
                 break;
             case States.stab:
+                attackMode = 1;
+                if(attack)
+                {                  
+                    charStates = States.swing;
+                   // ready = false;           
+                }
+                if (ready)
+                {
+                    charStates = States.idle;
+                }
                 break;
             case States.swing:
+                attackMode = 2;
+                if (ready)
+                {
+                    charStates = States.idle;          
+                }          
+                if (attack)
+                {
+                    charStates = States.stab;
+                  //  ready = false;
+                }
                 break;
         }
 	}
 
-
+    void CheckInput()
+    {
+        roll = Input.GetKeyDown(KeyCode.LeftShift);
+        attack = Input.GetMouseButtonDown(0);
+        jump = Input.GetKeyDown(KeyCode.Space);
+        possess = Input.GetMouseButtonDown(1);
+        hMove = Input.GetAxis("Horizontal");
+        vMove = Input.GetAxis("Vertical");
+    }
+    void MovementLogic()
+    {
+        Vector3 targetVelocity = new Vector3(hMove, 0, vMove);
+        targetVelocity.Normalize();
+        targetVelocity *= speed;
+        Vector3 velocity = _rigidBody.velocity;
+        Vector3 vChange = targetVelocity - velocity;
+        vChange = new Vector3(Mathf.Clamp(vChange.x,-speed, speed), 0, (Mathf.Clamp(vChange.z, -speed, speed)));
+        _rigidBody.AddForce(vChange, ForceMode.VelocityChange);
+        
+    }
+    void RotatingLogic(float h, float v)
+    {
+        Vector3 targetDir = new Vector3(h, 0, v);
+        Quaternion targetRot = Quaternion.LookRotation(targetDir,Vector3.up);
+        Quaternion newRot = Quaternion.Lerp(_rigidBody.rotation, targetRot, smoothDamp * Time.deltaTime);
+        _rigidBody.MoveRotation(newRot);
+    }
+    public void attackEnd()
+    {
+        ready = true;
+        Debug.Log("Attack ended");
+    }
+    public void attackStart()
+    {
+        ready = false;
+        Debug.Log("Attack started");
+    }
+    bool isGrounded()
+    {      
+        return Physics.Raycast(transform.position, -Vector3.up, 0.5f);
+    }
+    public void rollEnd()
+    {
+        isRolling = false;
+    }
 }
