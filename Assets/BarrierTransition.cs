a5a60a2f9fcaf24acf821e25678388f7c6df0eee
Amadeus Chia <foodpunch@gmai.com>
2015-07-20 01:28:34 +0800

    Redid Barrier Shenanigans, Added level triggers, Added the transition scenes and Camera Shakes.

---
+++ Assets/BarrierTransition.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class BarrierTransition : MonoBehaviour {
+
+    public GameObject _hideThis;
+    public float duration;
+    CameraShake shakeMe;
+	// Use this for initialization
+	void Start () {
+        shakeMe = GameObject.FindGameObjectWithTag("MainCamera").GetComponent<CameraShake>();
+        shakeMe.ShakeMeBaby(duration -0.5f);
+        Invoke("HideMe", duration +0.5f);
+	}
+	
+	// Update is called once per frame
+	void Update () {
+     
+	
+	}
+    void HideMe()
+    {
+        _hideThis.SetActive(false);
+
+    }
+}
