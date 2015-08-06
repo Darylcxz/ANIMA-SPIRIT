5e80fad823c0a2d56458e2e3305a9cd0bef24c17
Darylcxz <dcxz96@live.com>
2015-08-06 15:17:57 +0800

    re-re-rearrange

---
+++ Assets/Scripts/Underground1LevelController.cs
@@ -0,0 +1,32 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Underground1LevelController : MonoBehaviour {
+
+    public GameObject _destroyable;
+    public bool isExplosion;
+    public bool isDead;
+
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        CheckExplosion();
+	}
+
+    void CheckExplosion()
+    {
+        if (isExplosion)
+        {
+            _destroyable.SetActive(false);
+        }
+        else if (!isExplosion)
+        {
+            _destroyable.SetActive(true);
+        }
+    }
+}
---
+++ Assets/Scripts/script archive/MovableObjects.cs
@@ -0,0 +1,46 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class MovableObjects : MonoBehaviour {
+	
+	public GameObject statue;
+	private GameObject followpos;
+	private bool isAttached = false;
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void OnTriggerStay (Collider other) {
+
+		if (other.gameObject.name == "Character") {
+			if(Input.GetKeyDown("e") && charactermovement.isBeingControlled == true)
+			{
+				followpos = GameObject.Find("Pullobj");
+				isAttached = true;
+				charactermovement.isBeingControlled = false;
+				NewCharacterMovement.isControlling = true;
+				print ("attach");
+			}
+
+			else if(Input.GetKeyDown("e") && charactermovement.isBeingControlled == false)
+			{
+				print("detach");
+				charactermovement.isBeingControlled = true;
+				NewCharacterMovement.isControlling = false;
+				isAttached = false;
+			}
+
+		}
+
+	
+	}
+
+	void Update() {
+
+		if (isAttached)
+			statue.transform.position = followpos.transform.position;
+	}
+}
