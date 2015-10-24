8acd7ca3e595b317eb6535253ea4ae344266686b
Amadeus Chia <foodpunch@gmai.com>
2015-10-24 11:43:26 +0800

    Added Clouds and made them move and spawn at random positions and stuff. Might need cloud Variations

---
+++ Assets/CloudMove.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CloudMove : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		gameObject.transform.localPosition += transform.right/35;
+	}
+}
