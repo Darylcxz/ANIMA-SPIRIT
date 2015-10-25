8979e071e6a55fa550ab74b3a6d709eef7981983
Amadeus Chia <foodpunch@gmai.com>
2015-10-26 02:00:57 +0800

    UI appearing on some objects (needs to be changed in the future)

--- Assets/Scripts/GulnazGrab.cs
+++ Assets/Scripts/GulnazGrab.cs
@@ -1,4 +1,5 @@
 ﻿using UnityEngine;
+using UnityEngine.UI;
 using System.Collections;
 
 public class GulnazGrab : MonoBehaviour {
@@ -9,19 +10,43 @@ public class GulnazGrab : MonoBehaviour {
     private Vector3 side1;
     private Vector3 side2;
     private RaycastHit hit;
+
+	public Canvas _imgBubble;
+	public Canvas _imgHand;
+	public Canvas _imgFire;
+
+	bool spawnUI;
+	bool spawnUI1;
+
 	// Use this for initialization
 	void Start () {
 	
 	}
 	// Update is called once per frame
 	void Update () {
-
         center = transform.position + new Vector3(0, 0.5f, 0);
         side1 = center + new Vector3(0.2f, 0, 0);
         side2 = center + new Vector3(-0.2f, 0, 0);
 		Debug.DrawRay(center, transform.forward );
 		Debug.DrawRay(side1, transform.forward );
 		Debug.DrawRay(side2, transform.forward );
+		if (Physics.Raycast(center, transform.forward, out hit, 2f) || Physics.Raycast(side1, transform.forward, out hit, 2f) || Physics.Raycast(side2, transform.forward, out hit, 2f))
+		{
+			if (hit.collider.tag == "lever" && spawnUI == false)
+			{
+				hit.collider.transform.GetChild(0).GetComponent<Canvas>().enabled = true;
+				spawnUI = true;
+			}
+			//if(hit.collider.tag=="movable" && spawnUI == false)
+			//{
+			//	hit.collider.transform.GetChild(0).GetComponent<Canvas>().enabled = true;
+			//}
+			if (hit.collider.tag == "log" && spawnUI1 == false)
+			{
+				hit.collider.transform.GetChild(0).GetComponent<Canvas>().enabled = true;
+				spawnUI1 = true;
+			}
+		}
 
         if (Physics.Raycast(center, transform.forward, out hit, 0.5f) && Input.GetButtonDown("Action") || Physics.Raycast(side1, transform.forward, out hit, 0.5f) && Input.GetButtonDown("Action") || Physics.Raycast(side2, transform.forward, out hit, 0.5f) && Input.GetButtonDown("Action"))
         {
@@ -40,11 +65,13 @@ public class GulnazGrab : MonoBehaviour {
 			{
  				hit.collider.gameObject.GetComponent<Animator>().SetBool("bLever",true);
 				GameObject.Find("gate").GetComponent<Animator>().SetBool("bLever", true);
+				hit.collider.transform.GetChild(0).GetComponent<Canvas>().enabled = false;
 			}
 			if (hit.collider.tag == "log")
 			{
 				hit.collider.gameObject.GetComponent<Animator>().SetBool("bLog", true);
 				GameObject.FindGameObjectWithTag("log").GetComponent<Animator>().SetBool("bLog", true);
+				hit.collider.transform.GetChild(0).GetComponent<Canvas>().enabled = false;
 			}
         }
 		
