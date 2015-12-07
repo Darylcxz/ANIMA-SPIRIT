867c6d54e6efe397b6d3f4c81ba6faa6a68c111d
Amadeus Chia <foodpunch@gmai.com>
2015-12-07 10:53:20 +0800

    Changing scenes for caves

---
+++ Assets/CaveExit.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CaveExit : MonoBehaviour {
+	public int caveNum;
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+	void OnTriggerEnter(Collider col)
+	{
+		if (col.tag == "Player")
+		{
+			PlayerPrefs.SetInt("caveExit", caveNum);
+			Application.LoadLevel("1-2 Exterior");
+		}
+	}
+}
